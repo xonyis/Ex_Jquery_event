c8b2eeb32274867e4049e84f647d27f1cbc663ad
Julian <julian@MacBook-Pro-de-Julian.local>
2023-03-07 11:38:00 +0100

    Ex4 fini

--- jquery/jquery.event4.js
+++ jquery/jquery.event4.js
@@ -1,16 +1,25 @@
 $(function() {
     $('#rgbSub').on('click', function(event) {
-        let rouge = $('#rgb1').val();
-        let bleu = $('#rgb2').val();
-        let vert = $('#rgb3').val();
-        
-        if (rouge < 255 || bleu < 255 || vert < 255) {
-            console.log('erroe');
-        } else {
-            $(".colorContainer").css('background', 'rgb('+rouge+','+bleu+','+vert+')');
+        if ($('#rgb1').val()=="") {
+            alert("veuillez remplir le champ rouge")
+        }
+
+        if ($('#rgb2').val()=="") {
+            alert("veuillez remplir le champ vert")
+        }
+
+        if ($('#rgb3').val()=="") {
+            alert("veuillez remplir le champ bleu")
         }
 
 
+        if ($('#rgb1').val() > 255 || $('#rgb2').val() > 255 || $('#rgb3').val() > 255) {
+            alert("entre 0 et 255")
+        } else {
+            $('.colorContainer').css("background-color", "rgb("+ $('#rgb1').val() +","+ $('#rgb2').val()+","+ $('#rgb3').val()+")");
+        }
+       
     })
 
 })
+
